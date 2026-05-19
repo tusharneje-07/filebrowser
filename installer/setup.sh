ee71cebea6ac1242b3edbf48e30717c05449ad97
Tushar Neje <nejetushar07@gmail.com>
2026-05-19 21:16:57 +0530

    chore: silence setup.sh logs for a cleaner install experience

--- installer/setup.sh
+++ installer/setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # Usage: curl -fsSL https://raw.githubusercontent.com/tusharneje-07/file_browser/main/installer/setup.sh | bash
-echo "Installer start..."
 OS="$(uname)"
 case "$OS" in
     Linux*|Darwin*)
-        echo "Detected $OS. Starting installation..."
         bash <(curl -fsSL https://raw.githubusercontent.com/tusharneje-07/file_browser/main/installer/install.sh)
         ;;
     *)
