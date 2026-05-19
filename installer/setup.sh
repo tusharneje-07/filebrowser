48af392c45aff335030eb0deee77901644e3a123
Tushar Neje <nejetushar07@gmail.com>
2026-05-19 20:21:30 +0530

    fix: explicit update to setup.sh

--- installer/setup.sh
+++ installer/setup.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-# Universal entry point for curl | bash
 # Usage: curl -fsSL https://raw.githubusercontent.com/tusharneje-07/file_browser/main/installer/setup.sh | bash
-
+echo "Installer start..."
 OS="$(uname)"
 case "$OS" in
     Linux*|Darwin*)
-        echo "Detected $OS. Starting installation from GitHub..."
+        echo "Detected $OS. Starting installation..."
         bash <(curl -fsSL https://raw.githubusercontent.com/tusharneje-07/file_browser/main/installer/install.sh)
         ;;
     *)
         echo "Unsupported OS: $OS"
-        echo "For Windows, please run the PowerShell installer found in the repository."
         exit 1
         ;;
 esac
